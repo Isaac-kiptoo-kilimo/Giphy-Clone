0149f07a35eb8e8a5f5a6a123983e8cee74f04e8
Isaac-kiptoo-kilimo <isaac.kilimo@student.moringaschool.com>
2022-04-14 14:08:47 +0300

    feature:This will add the navbar and the divs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GifsComponent } from './components/gifs/gifs.component';
+import { GifDetailsComponent } from './components/gif-details/gif-details.component';
+import { NavbarComponent } from './navbar/navbar/navbar.component';
+import { FormComponent } from './forms/form/form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    GifsComponent
+    GifsComponent,
+    GifDetailsComponent,
+    NavbarComponent,
+    FormComponent
   ],
   imports: [
     BrowserModule,
